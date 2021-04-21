32eef75648585c110acb3fb4e2b6b0e50e15b6dc
Doc Mo <maueroats@gmail.com>
2021-04-21 17:37:00 -0500

    wy-cs site update 2021-04-21 17:37

--- publish.sh
+++ publish.sh
@@ -4,6 +4,7 @@ if [ -z "$base_dir" ]; then echo "$0: must set base_dir!" 1>&2 ; exit 1 ; fi
 
 echo "WORKING: ${base_dir}"
 cd "$base_dir"
+hugo version
 hugo --cleanDestinationDir
 git add -A
 git commit -a -m "wy-cs site update $(date +'%Y-%m-%d %H:%M')"
